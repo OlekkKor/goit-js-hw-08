4d1ed2809832c4620ecd8271fba686bdf21bbc55
Oleksandr Korovin <112637896+OlekkKor@users.noreply.github.com>
2022-11-10 22:42:40 +0200

    Task 1,2,3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,45 @@
+import throttle from 'lodash.throttle';
 
+const LOCAL_KEY = "feedback-form-state"; 
+let form = document.querySelector('.feedback-form');
+// змінна визначена як let тому що очищається у 43 рядку
+
+
+let userData = {};
+ 
+form.addEventListener('input', throttle(onInputValue, 2000));
+form.addEventListener('submit', onContactFormSubmit);
+
+
+function onInputValue(e) {
+
+  const name = e.target.name;
+  const value = e.target.value;
+
+  userData[name] = value;
+
+  localStorage.setItem(LOCAL_KEY, JSON.stringify(userData));
+}
+
+function fillContactFormElements() {
+    const userDataFromLS = JSON.parse(localStorage.getItem(LOCAL_KEY));
+    console.log(userDataFromLS);
+        for (const prop in userDataFromLS) {
+            if (userDataFromLS.hasOwnProperty(prop)) {
+                 console.log(prop);
+
+            form.elements[prop].value = userDataFromLS[prop];
+            }
+        }
+    };
+
+fillContactFormElements();
+
+function onContactFormSubmit (event) {
+
+    event.preventDefault();
+  
+    event.currentTarget.reset();
+    form = {};
+    localStorage.removeItem(LOCAL_KEY);
+  };
